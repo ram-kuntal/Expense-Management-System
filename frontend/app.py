eaffab22d29d4f5e81318b41f513d14e08592407
jEDI <ramkuntal2002@gmail.com>
2025-08-14 18:38:42 +0530

    Add files via upload

---
+++ frontend/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from add_update_ui import add_update_tab
+from analytics_category_ui import analytics_category_tab
+from analytics_month_ui import analytics_month_tab
+
+API_URL = "http://localhost:8000"
+
+st.title("Expense Management System")
+
+tab1, tab2, tab3 = st.tabs(["Add/Update", "Analytics by Category", "Analytics by Month"])
+
+with tab1:
+    add_update_tab()
+with tab2:
+    analytics_category_tab()
+with tab3:
+    analytics_month_tab()
+
+
+
